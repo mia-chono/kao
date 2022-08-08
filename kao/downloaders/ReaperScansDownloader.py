ee83d61a3cae295f74657d002d0925eafb6f7cb8
Mia Chono <mia.chono@proton.me>
2022-08-08 23:48:09 +0200

    fix ReaperScansDownloader broken links and crawler

--- kao/downloaders/ReaperScansDownloader.py
+++ kao/downloaders/ReaperScansDownloader.py
@@ -1,6 +1,7 @@
 import re
 
 from .Downloader import Downloader
+from .. import utils
 
 
 class ReaperScansDownloader(Downloader):
@@ -11,17 +12,17 @@ class ReaperScansDownloader(Downloader):
 
     @staticmethod
     def is_a_series_link(link: str) -> bool:
-        return re.search(r"https?://(www\.)?reaperscans\.fr/manga/((\w*-*)+\d*)/?$", link) is not None
+        return re.search(r"https?://(www\.)?reaperscans\.fr/series/[^/?]+/?$", link) is not None
 
     @staticmethod
     def is_a_chapter_link(link: str) -> bool:
-        return re.search(r"https?://(www\.)?reaperscans\.fr/((\w*-*)+\d*)/?$", link) is not None
+        return re.search(r"https?://(www\.)?reaperscans\.fr/series/[^/]+/chapitre-\d+/?$", link) is not None
 
     @staticmethod
     def _extract_pictures_links_from_webpage(dom) -> list[str]:
-        img_tags = dom.xpath("//*[@id='readerarea']//img")
+        img_tags = dom.xpath('//*[@class="reading-content"]//img')
 
-        pictures_links = list(map(lambda img_tag: img_tag.get("src"), img_tags))
+        pictures_links = list(map(lambda img_tag: img_tag.get("src").replace("\n", ""), img_tags))
 
         return pictures_links
 
@@ -29,31 +30,30 @@ class ReaperScansDownloader(Downloader):
         if link[len(link) - 1] != "/":
             link += "/"
 
-        for logger in self.loggers:
-            logger.log("[Info][{}][Series] Get HTML content".format(self.platform))
+        utils.log(self.loggers, "[Info][{}][Series] Get HTML content".format(self.platform))
 
         soup, dom = self._get_page_content(link)
 
-        series_title = soup.find("h1", {"class": "entry-title"})
+        series_title = soup.find("div", {"class": "post-title"}).find("h1").text.rstrip().replace("\n", "")
 
         series = self.generate_series(series_title, link)
 
         # get all website url of the series
-        for chapter_tag in soup.find("div", {"id": "chapterlist"}).find_all("li"):
+        for chapter_tag in soup.find_all("div", {"class": "chapter-link"}):
             series.add_chapter_link(chapter_tag.find("a").attrs["href"])
         series.get_chapters_links().reverse()
 
         series = self._download_chapters_from_series(series, force_re_dl, keep_img)
 
-        for logger in self.loggers:
-            logger.log("[Info][{}][series] '{}': completed".format(self.platform, series.get_name()))
+        utils.log(self.loggers, "[Info][{}][series] '{}': completed".format(self.platform, series.get_name()))
 
         return series
 
     def download_chapter(self, link: str, force_re_dl: bool = False, keep_img: bool = False):
         soup, dom = self._get_page_content(link)
 
-        series_title = dom.xpath("/html/body/div[2]/div[2]/div[1]/div/article/div[1]/div/a")[0].text
-        series_chapter = soup.find("div", {"class": "daw chpnw"}).text.rstrip()
+        series_title = soup.select_one(".breadcrumb > li:nth-child(2)").text.rstrip().replace("\n", "")
+        series_chapter = soup.select_one(".breadcrumb > .active").text.rstrip().replace("\n", "")
 
-        return self._download_chapter_files(dom, series_title, series_chapter, 'http://reaperscans.fr/', force_re_dl, keep_img)
+        return self._download_chapter_files(dom, series_title, series_chapter, 'https://reaperscans.fr/', force_re_dl,
+                                            keep_img)
