53076288b45a3dbca213171a6f488a0505e82580
Mia Chono <mia.chono@proton.me>
2022-08-08 23:50:04 +0200

    refactor move-pdf command and refactor to remove shadows name from outer scope

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 # KAO
 ## Console Help
 ```bash
-usage: __main__.py [-h] [-l LINKS [LINKS ...]] [-f | --force | --no-force] [-s | --support | --no-support]
+usage: __main__.py [-h] [-l LINKS [LINKS ...]] [-k | --keep-img | --no-keep-img] [-f | --force | --no-force] [-m [MOVE_PDF]] [-r [READ_FILE]] [-s | --support | --no-support]
 
 Downloader of manwha or manga scans
 
 options:
   -h, --help            show this help message and exit
   -l LINKS [LINKS ...], --links LINKS [LINKS ...]
-                        Give chapters or series links
+                        Give chapters or series links (example2: py __main__.py -l link1 link2) (example2: py __main__.py -l link1 link2 -r file -m)
   -k, --keep-img, --no-keep-img
-                        If you want keep all images (default: False)
+                        If you want keep all images after download (example: py __main__.py -fkl link) (example2: py __main__.py -l link -r file -m) (default: False)
   -f, --force, --no-force
-                        Download again the scan (default: False)                 
-  -p, --pdf, --no-pdf   Move all pdf files to pdf folder (folder will be created if not exists at the root of the downloads folder) (default: False)
+                        Download again the scan (example: py __main__.py -fkl link) (example2: py __main__.py -l link -r file -m) (default: False)
+  -m [MOVE_PDF], --move-pdf [MOVE_PDF]
+                        Move all pdf files to pdf folder (folder will be created if not exists at the root of the downloads folder), put ALWAYS at the end of command to move all pdf files
+  -r [READ_FILE], --Read-file [READ_FILE]
+                        Read given file to get urls, default is './list url.txt' but you can specify another (example: py __main__.py -fkr file) (example2: py __main__.py -l link -r file -m)
   -s, --support, --no-support
                         Said supported websites (default: False)
+
 ```
 
 ## Supported Sites
@@ -27,10 +31,14 @@ options:
 
 > *information*: you can download again a same scan with force parameter
 ## Group all PDF in one directory
-* update 2022.08.07
-When you use the property --pdf, all pdf files will be moved to a folder named pdf in downloads folder.
+* update 2022.08.07  
+When you use the property --move-pdf, all pdf files will be moved to a folder named pdf.    
+the pdf folder will be created if not exists at the root of the downloads folder if you don't give a path.  
+   
+if you give a specific path to move all pdf (including sub-folders), the pdf folder will be created if not exists at the path given earlier.
 
-Example:
+### Example 1 - no given path
+Example for `py __main__.py -m "/path/to"`:
 ```
 /path/to/manga/chap-01
  |_ chap-01.pdf
@@ -39,8 +47,9 @@ Example:
 /path/to/manga2/chap-01
  |_ chap-01.pdf
 ```
+> pdf files will be moved to the folder `pdf` at the root of the downloads folder.
 
-Result:
+Result of `py __main__.py -m "/path/to"`:
 ```
 /downloads/pdf/manga
  |_ chap-01.pdf
@@ -48,7 +57,26 @@ Result:
 /downloads/pdf/manga2
  |_ chap-01.pdf
 ```
+### Example 2 - given path
 
+Example for `py __main__.py -m "/path/to/manga"`:
+```
+/path/to/manga/chap-01
+ |_ chap-01.pdf
+/path/to/manga/chap-02
+ |_ chap-02.pdf
+```
+> pdf files will be moved to the folder `pdf` at the path given earlier.
+
+Result of `py __main__.py -m "/path/to/manga"`:
+```
+/path/to/manga
+ |_ /chap-01
+ |_ /chap-02
+ |_ /pdf
+     |_ chap-01.pdf
+     |_ chap-02.pdf
+```
 ## Personal folders
 * update 2022.08.07  
 Fix image conversion to pdf. Now we ensure image format.
--- __main__.py
+++ __main__.py
@@ -1,5 +1,8 @@
 import os
 import argparse
+from typing import Iterator
+
+import validators
 
 from kao import utils
 from kao import Chapter
@@ -30,7 +33,7 @@ list_downloaders: [Downloader] = [
 ]
 
 
-def download_series(series_to_download: list[str], force_re_dl: bool, keep_img: bool) -> Chapter:
+def download_series(series_to_download: list[dict[str, str]], force_re_dl: bool, keep_img: bool) -> Iterator[Chapter]:
     for obj in series_to_download:
         for downloader in list_downloaders:
             if downloader.platform == obj.get("platform"):
@@ -39,14 +42,14 @@ def download_series(series_to_download: list[str], force_re_dl: bool, keep_img: 
                     yield chapter
 
 
-def download_chapters(chapter_to_download: list[str], force_re_dl: bool, keep_img: bool) -> Chapter:
+def download_chapters(chapter_to_download: list[dict[str, str]], force_re_dl: bool, keep_img: bool) -> Iterator[Chapter]:
     for downloader in list_downloaders:
         for obj in chapter_to_download:
             if downloader.platform == obj.get("platform"):
                 yield downloader.download_chapter(obj.get("link"), force_re_dl, keep_img)
 
 
-def get_series_and_chapters_links(links: list[str]) -> tuple[list[str], list[str]]:
+def get_series_and_chapters_from_links(links: list[str]) -> tuple[list[dict[str, str]], list[dict[str, str]]]:
     correct_links = []
     series_to_download = []
     chapters_to_download = []
@@ -68,14 +71,14 @@ def get_series_and_chapters_links(links: list[str]) -> tuple[list[str], list[str
         for logger in loggers:
             logger.log("Invalid link(s), please give links from {}\n{}".format(
                 ", ".join(x.platform for x in list_downloaders),
-                "\n".join(link for link in links_not_available)
+                "\n".join(link_not_available for link_not_available in links_not_available)
             ))
 
     return series_to_download, chapters_to_download
 
 
-def download_all_chapters(series_to_download: list[str], chapters_to_download: list[str], force_re_dl: bool,
-                          keep_img: bool) -> list[str]:
+def download_all_chapters(series_to_download: list[dict[str, str]], chapters_to_download: list[dict[str, str]],
+                          force_re_dl: bool, keep_img: bool) -> list[Chapter]:
     chapters = []
     for chapter in download_chapters(chapters_to_download, force_re_dl, keep_img):
         chapters.append(chapter)
@@ -86,30 +89,48 @@ def download_all_chapters(series_to_download: list[str], chapters_to_download: l
     return chapters
 
 
+def download_links(links_to_dl: list[str], force_re_dl: bool, keep_img: bool) -> None:
+    series_links, chapters_links = get_series_and_chapters_from_links(links_to_dl)
+    download_all_chapters(series_links, chapters_links, force_re_dl, keep_img)
+    for logger in loggers:
+        logger.log("[Success] downloads completed")
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Downloader of manwha or manga scans')
     parser.add_argument("-l",
                         "--links",
                         nargs="+",
                         dest="links",
-                        help="Give chapters or series links")
+                        help="Give chapters or series links (example2: py __main__.py -l link1 link2) "
+                             "(example2: py __main__.py -l link1 link2 -r file -m)")
     parser.add_argument("-k",
                         "--keep-img",
                         dest="keep_img",
-                        help="If you want keep all images",
+                        help="If you want keep all images after download (example: py __main__.py -fkl link) "
+                             "(example2: py __main__.py -l link -r file -m)",
                         action=argparse.BooleanOptionalAction,
                         default=False)
     parser.add_argument("-f",
                         "--force",
                         dest="force_re_dl",
-                        help="Download again the scan",
+                        help="Download again the scan (example: py __main__.py -fkl link) "
+                             "(example2: py __main__.py -l link -r file -m)",
                         action=argparse.BooleanOptionalAction,
                         default=False)
-    parser.add_argument("-p",
-                        "--pdf",
+    parser.add_argument("-m",
+                        "--move-pdf",
                         dest="move_pdf",
-                        help="Move all pdf files to pdf folder (folder will be created if not exists at the root of the downloads folder)",
-                        action=argparse.BooleanOptionalAction,
+                        nargs="?",
+                        help="Move all pdf files to pdf folder (folder will be created if not exists at the root of "
+                             "the downloads folder), put ALWAYS at the end of command to move all pdf files",
+                        default=False)
+    parser.add_argument("-r",
+                        "--Read-file",
+                        dest="read_file",
+                        nargs="?",
+                        help="Read given file to get urls, default is './list url.txt' but you can specify another "
+                             "(example: py __main__.py -fkr file) (example2: py __main__.py -l link -r file -m)",
                         default=False)
     parser.add_argument("-s",
                         "--support",
@@ -123,18 +144,30 @@ if __name__ == '__main__':
     if args.support:
         print("Supported websites:\n\t{}".format(", ".join([downloader.platform for downloader in list_downloaders])))
 
+    # TODO add in the folder of each manga a file "downloaded_chapters.txt" with the name of the downloaded chapters
+    #  or the link...
     if args.links:
-        links = args.links
-        for index, link in enumerate(links):
-            if link[-1] == '"':
-                links[index] = link[:-1]
+        links_cli = args.links
+        for index, tmp_link in enumerate(links_cli):
+            if tmp_link[-1] == '"':
+                links_cli[index] = tmp_link[:-1]
+        download_links(links_cli, args.force_re_dl, args.keep_img)
 
-        series_links, chapters_links = get_series_and_chapters_links(links)
-        download_all_chapters(series_links, chapters_links, args.force_re_dl, args.keep_img)
-        for logger in loggers:
-            logger.log("[Success] downloads completed")
+    if args.read_file is not False:
+        file_with_links = os.path.abspath(
+            args.read_file if args.read_file is not None else "list url.txt")
 
-    if args.move_pdf:
-        destination_dir = os.path.join(base_dir, "pdf")
-        utils.create_directory(destination_dir)
-        utils.move_pdf_files_from_folder(base_dir, destination_dir, loggers)
+        with open(file_with_links) as f:
+            lines = [line.rstrip() for line in f]
+
+        download_links(lines, args.force_re_dl, args.keep_img)
+
+    if args.move_pdf is not False:
+        if args.move_pdf is not None and validators.url(args.move_pdf):
+            base_path = os.path.abspath(base_dir)
+        else:
+            base_path = os.path.abspath(args.move_pdf if args.move_pdf is not None else base_dir)
+        destination_dir = os.path.join(base_path, "pdf")
+
+        utils.move_pdf_files_from_folder(base_path, destination_dir, loggers)
+
