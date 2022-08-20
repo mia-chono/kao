f3f9c2e1bd32c0da0e38c0f8786432bb15b16985
Mia Chono <mia.chono@proton.me>
2022-08-20 21:21:05 +0200

    add a new invalid char for directory / files names => "

--- kao/utils.py
+++ kao/utils.py
@@ -17,12 +17,12 @@ invalid_file_name_chars = [
     chr(0), chr(1), chr(2), chr(3), chr(4), chr(5), chr(6), chr(7), chr(8), chr(9), chr(10), chr(11),
     chr(12), chr(13), chr(14), chr(15), chr(16), chr(17), chr(18), chr(19), chr(20), chr(21), chr(22), chr(23), chr(24),
     chr(25), chr(26), chr(27), chr(28), chr(29), chr(30), chr(31), chr(34), chr(60), chr(62), chr(124), ':', '*', '?',
-    '\\', '/'
+    '\\', '/', '"'
 ]
 invalid_directory_name_chars = [
     '|', '\0', chr(1), chr(2), chr(3), chr(4), chr(5), chr(6), chr(7), chr(8), chr(9), chr(10), chr(11),
     chr(12), chr(13), chr(14), chr(15), chr(16), chr(17), chr(18), chr(19), chr(20), chr(21), chr(22), chr(23), chr(24),
-    chr(25), chr(26), chr(27), chr(28), chr(29), chr(30), chr(31), ':', '*', '?', '\\', '/'
+    chr(25), chr(26), chr(27), chr(28), chr(29), chr(30), chr(31), ':', '*', '?', '\\', '/', '"'
 ]
 
 
